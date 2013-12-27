674faac4123b4026d83f487a4e0939716516e680
Ash Furrow <ash@ashfurrow.com>
2013-12-27 18:52:05 -0500

    Added (empty) ASHEditRecipeViewModelTests.

---
+++ C-41Tests/ASHEditRecipeViewModelTests.m
@@ -0,0 +1,34 @@
+//
+//  ASHEditRecipeViewModelTests.m
+//  C-41
+//
+//  Created by Ash Furrow on 12/27/2013.
+//  Copyright (c) 2013 Ash Furrow. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ASHEditRecipeViewModelTests : XCTestCase
+
+@end
+
+@implementation ASHEditRecipeViewModelTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
