94bb9491423584bad5291d84a5816e9c5dcfd464
Ash Furrow <ash@ashfurrow.com>
2014-01-05 13:35:27 -0500

    Finished writing tests.

--- C-41Tests/ASHEditStepViewModelTests.m
+++ C-41Tests/ASHEditStepViewModelTests.m
@@ -11,14 +11,37 @@
 #define EXP_SHORTHAND
 #import <Expecta/Expecta.h>
 #import <OCMock/OCMock.h>
+#import "CoreDataHelpers.h"
 
 #import "ASHEditStepViewModel.h"
 
 SpecBegin(ASHEditStepViewModel)
 
 describe(@"ASHEditStepViewModel", ^{
-    pending (@"correctly forwards model properties", ^{
+    static ASHStep *step;
+    
+    beforeEach(^{
+        NSManagedObjectContext *context = [[ASHCoreDataStack defaultStack] managedObjectContext];
+        [context reset];
         
+        ASHRecipe *recipe = setupRecipe(context);
+        step = recipe.steps[0];
+    });
+    
+    it (@"correctly forwards model properties", ^{
+        ASHEditStepViewModel *viewModel = [[ASHEditStepViewModel alloc] initWithModel:step];
+        
+        expect(viewModel.stepName).to.equal(step.name);
+        expect(viewModel.stepDescription).to.equal(step.blurb);
+        expect(viewModel.temperatureCelcius).to.equal(step.temperatureC);
+        expect(viewModel.duration).to.equal(step.duration);
+        expect(viewModel.agitationDuration).to.equal(step.agitationDuration);
+        expect(viewModel.agitationFrequency).to.equal(step.agitationFrequency);
+        
+        expect(viewModel.temperatureString).to.equal(@"39℃");
+        expect(viewModel.durationString).to.equal(@"For 1:00");
+        expect(viewModel.agitationDurationString).to.equal(@"Agitate for 0s");
+        expect(viewModel.agitationFrequencyString).to.equal(@"Agitate every 0s");
     });
 });
 
