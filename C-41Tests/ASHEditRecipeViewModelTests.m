17c663e8bbdf54ddfddb56f31e0f7a90beb8bdd7
Ash Furrow <ash@ashfurrow.com>
2014-01-05 11:48:09 -0500

    Basics tests for edit recipe view model.

--- C-41Tests/ASHEditRecipeViewModelTests.m
+++ C-41Tests/ASHEditRecipeViewModelTests.m
@@ -11,14 +11,28 @@
 #define EXP_SHORTHAND
 #import <Expecta/Expecta.h>
 #import <OCMock/OCMock.h>
+#import "CoreDataHelpers.h"
 
 #import "ASHEditRecipeViewModel.h"
 
 SpecBegin(ASHEditRecipeViewModel)
 
 describe(@"ASHEditRecipeViewModel", ^{
-    pending (@"it correctly gathers properties from the model", ^{
+    static ASHRecipe *recipe;
+    
+    beforeEach(^{
+        NSManagedObjectContext *context = [[ASHCoreDataStack defaultStack] managedObjectContext];
+        [context reset];
         
+        recipe = setupRecipe(context);
+    });
+    
+    it (@"it correctly gathers properties from the model", ^{
+        ASHEditRecipeViewModel *viewModel = [[ASHEditRecipeViewModel alloc] initWithModel:recipe];
+        
+        expect(viewModel.name).to.equal(recipe.name);
+        expect(viewModel.blurb).to.equal(recipe.blurb);
+        expect(viewModel.filmType).to.equal(recipe.filmType);
     });
     
     pending (@"cancel deletes the model iff inserting", ^{
